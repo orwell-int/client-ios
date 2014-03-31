dce7156776faa33c258b87f246d99b704a92a268
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-03-31 21:59:05 +0200

    Add a custom view controller

--- Project/src/init/ORViewController.h
+++ Project/src/init/ORViewController.h
@@ -24,56 +24,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "AppDelegate.h"
-#import "Game.h"
-#import "ORViewController.h"
-#import <DDLog.h>
-#import <DDTTYLogger.h>
-#import <DDASLLogger.h>
-#import <DDFileLogger.h>
+#import "SPViewController.h"
 
-// --- c functions ---
-
-void onUncaughtException(NSException *exception)
-{
-    NSLog(@"uncaught exception: %@", exception.description);
-}
-
-// ---
-
-@implementation AppDelegate
-{
-	ORViewController *_viewController;
-	UIWindow *_window;
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    NSSetUncaughtExceptionHandler(&onUncaughtException);
-	
-	// Setup CocoaLumberjack for logging
-	[DDLog addLogger:[DDTTYLogger sharedInstance] withLogLevel:LOG_LEVEL_DEBUG];
-	[DDTTYLogger sharedInstance].colorsEnabled = YES;
-
-    CGRect screenBounds = [UIScreen mainScreen].bounds;
-    _window = [[UIWindow alloc] initWithFrame:screenBounds];
-    
-    _viewController = [[ORViewController alloc] init];
-	_viewController.showStats = NO;
-	_viewController.multitouchEnabled = YES;
-	_viewController.preferredFramesPerSecond = 60;
-    
-    [_viewController startWithRoot:[Game class] supportHighResolutions:YES doubleOnPad:YES];
-    
-    [_window setRootViewController:_viewController];
-    [_window makeKeyAndVisible];
-
-    return YES;
-}
-
-- (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
-{
-	return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscape;
-}
+@interface ORViewController : SPViewController
 
 @end
